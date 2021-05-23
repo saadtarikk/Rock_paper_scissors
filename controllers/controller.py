c45fc9a10da871d2ecd8418e5f209308ecbfa351
[master] <saad.tarik@outlook.com>
2021-05-23 22:38:50 +0100

    removed comments

--- controllers/controller.py
+++ controllers/controller.py
@@ -19,10 +19,6 @@ def rock_paper_scissors(player1_choice, player2_choice):
     player1 = Player("Saad", player1_choice)
     player2 = Player("Lukas", player2_choice)
     winner = Game().game_winner(player1, player2)
-    # if winner:
-    #     result = f"{winner} wins by playing {winner}"
-    # else:
-    #     result = "its a draw"
     return render_template('result.html', player1=player1, player2=player2, winner=winner)
 
 
@@ -36,10 +32,7 @@ def get_player1_name():
     title = "Play Game"
     player1 = Player(request.form["player1_name"],
                      request.form["player1_selection"])
-
     random_choice = Game().computer_random_choice()
     player2 = Player("Computer", random_choice)
-
     winner = Game().game_winner(player1, player2)
-
     return render_template("play_game.html", title=title, player1_name=player1.name, player1_choice=player1.choice, player2_name=player2.name, player2_choice=player2.choice, winner=winner)
